39eda370f345b0738790398e56233d52c9ba92b0
yashal-ali <yashalalifarooqui30@gmail.com>
2025-03-04 16:37:56 +0500

    types

--- src/types/next-auth-package.d.ts
+++ src/types/next-auth-package.d.ts
@@ -7,4 +7,13 @@ declare module "next-auth-package" {
   export const authOptions: any;
   export function dbConnect(): void;
   export const UserModel: any;
+    export function registerUser(params: {
+      firstName: string;
+      lastName: string;
+      userName: string;
+      email: string;
+      password: string;
+      domain: string;
+      EmailComponent: React.ElementType;
+    }): Promise<{ success: boolean; message: string }>;
 }
